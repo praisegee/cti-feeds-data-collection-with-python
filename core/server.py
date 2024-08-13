f91ec253fe22e15eaa5ab890bf04b55addc8ae5f
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2024-08-13 14:02:37 +0100

    ready to deploy

--- core/server.py
+++ core/server.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify
 
+from core.database import Base, engine
+from core.feeds import fetch_feeds
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    # TODO: make request to CTI feeds APIs
-    return jsonify({"message": "Fetching...", "status": True})
+    # Create all tables defined by the models
+    Base.metadata.create_all(engine)
+
+    try:
+        fetch_feeds()
+        return jsonify({"message": "Success", "status": True})
+    except Exception:
+        return jsonify({"message": "Fail", "status": False})
