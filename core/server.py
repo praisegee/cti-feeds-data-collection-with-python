479a8c1fdf13fdc5e61233a12b096ddeea62f06b
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2024-08-13 15:50:37 +0100

    ready to deploy

--- core/server.py
+++ core/server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 
 from core.database import Base, engine
@@ -6,13 +8,19 @@ from core.feeds import fetch_feeds
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/load")
 def index():
-    # Create all tables defined by the models
-    Base.metadata.create_all(engine)
-
     try:
         fetch_feeds()
         return jsonify({"message": "Success", "status": True})
     except Exception:
         return jsonify({"message": "Fail", "status": False})
+
+
+if __name__ == "__main__":
+    # Create all tables defined by the models
+    Base.metadata.create_all(engine)
+
+    host = os.getenv("SERVER_HOST", "localhost")
+    port = os.getenv("SERVER_PORT", 8000)
+    app.run(debug=True, port=port, host=host)
